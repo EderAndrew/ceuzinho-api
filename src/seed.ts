debf56203beb5b75aea0fb28995682a2cb3c7bb4
Eder Andrew <eder.silva@montreal.com.br>
2025-09-04 12:05:48 -0300

    Update for building

---
+++ src/seed.ts
@@ -0,0 +1,34 @@
+import { PrismaClient, Role, Sex } from "@prisma/client"
+import { getBackgroundColorBySex } from "../src/modules/users/utils/userUtils";
+import { hashSync } from "bcrypt";
+import { PASSWORD_CONFIG } from "../src/modules/users/utils/constants";
+const prisma = new PrismaClient()
+
+const hashedPassword = hashSync(process.env.SEED_PASSWORD as string, PASSWORD_CONFIG.SALT_ROUNDS);
+
+async function main() {
+    const user = await prisma.user.upsert({
+        where: { email: "ederandrew0028@gmail.com" },
+        update: {},
+        create: {
+            name: process.env.SEED_NAME as string,
+            email: process.env.SEED_EMAIL as string,
+            password: hashedPassword,
+            phone: process.env.SEED_PHONE as string,
+            role: process.env.SED_ROLE as Role,
+            sex: process.env.SEED_SEX as Sex,
+            bgColor: getBackgroundColorBySex(process.env.SEED_SEX as Sex),
+        }
+    })
+    console.log(`User created: ${user}`)
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
